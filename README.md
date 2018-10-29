d66734a04b6fb60df29d9b49e42fec22a1fb9016
Ben Lopata <blopata@spp.org>
2018-10-29 16:39:55 -0500

    Encapsulated movieTable and tableHeader in components. Added pagination, filtering, and sorting. Updated readme.

--- README.md
+++ README.md
@@ -54,3 +54,96 @@ Whenever you need to pass an argument, simply define the argument as an input to
 ```js
 onClick={() => this.handleIncrement(product)}
 ```
+
+## Lifting the state
+
+Previously, our `index.js` file was rendering only the Counters class in the ReactDOM. With the addition of a NavBar which required the count of total active counters, the state of the `Counters` class object needing to be lifted to the `App.js` file along with the `NavBar`. Similar to implementing `Counters` which inherited from `Counter`, the `Counters` class now only passes events --known as "bubbling up" events-- such as `onDelete`, `onIncrement` through props to the `App.js` file to be rendered in the DOM.
+
+## Stateless functional component
+
+Rather than using a class, you can use a stateless functional component (**sfc**). For example in our navbar component, which had a `NavBar` class, can be represented by the following sfc
+
+```js
+const NavBar = props => {
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <a className="navbar-brand">
+        Navbar{" "}
+        <span className="badge badge-pill secondary">
+          {props.totalCounters}
+        </span>
+      </a>
+    </nav>
+  );
+};
+```
+
+Note that the props were passed in as an input to the arrow function rather than referencing `this.props` in the Class implementation.
+
+## Object destructuring
+
+Rather than referencing `this.props` numerous times, you can use object destructuring to simplify the code for passing the props. for instance, in our `counters.jsx` component,
+
+```js
+const { onReset, onDelete, counters, onIncrement } = this.props;
+```
+
+and the references to `this.props.onReset` become simply `onReset`.
+
+## Lifecycle Hooks
+
+Components go through several phases in their lifecycle.
+The first is the Mount phase, which has three lifecycle hooks:
+
+- constructor
+- render
+- componentDidMount
+
+Next is the update phase, with two hooks:
+
+- render
+- componentDidUpdate
+
+Finally is the unmounting phase, which has one hook:
+
+- componentWillUnmount
+
+## Mounting phase
+
+The Constructor property is called once upon mounting an object in the DOM. The state can be set directly in the constructor. This is the correct place to initialize the properties.
+
+### componentDidMount
+
+This method is called once after the component is mounted in the DOM. This is the place to make Ajax calls and set the state with data from the server.
+
+### Render
+
+`render()` is called when React renders the object returned from the VirtualDOM in the ReactDOM, after which the component is mounted - or contained within the React DOM. When a component is rendered, all of the children are rendered recursively. The output of our App in the console is as follows:
+
+```
+App - Constructor
+App - Rendered
+NavBar - Rendered
+Counters - Rendered
+(5)Counter - Rendered
+App - Mounted
+```
+
+## Updating phase
+
+## Pagination
+
+## Type checking
+
+Using the `prop-types` package, we can added type checking to our pagination component like so:
+
+```js
+Pagination.propTypes = {
+  itemsCount: PropTypes.number.isRequired,
+  pageSize: PropTypes.number.isRequired,
+  onPageChange: PropTypes.func.isRequired,
+  currentPage: PropTypes.number.isRequired
+};
+```
+
+Adding type checking also adds documentation to props required in the app, their types, and if they are required or not.
--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,16 +1,38 @@
 import React, { Component } from "react";
 import { getMovies } from "../services/fakeMovieService";
-import Like from "./common/like";
+import MoviesTable from "./moviesTable";
+import Pagination from "./common/pagination";
+import { paginate } from "../utils/paginate";
+import ListGroup from "./common/listGroup";
+import { getGenres } from "../services/fakeGenreService";
+import _ from "lodash";
 
 class Movies extends Component {
   state = {
-    movies: getMovies()
+    movies: [],
+    genres: [],
+    pageSize: 4,
+    currentPage: 1,
+    sortColumn: { path: "title", order: "asc" }
+  };
+
+  componentDidMount() {
+    const genres = [{ _id: "", name: "All Genres" }, ...getGenres()];
+    this.setState({ movies: getMovies(), genres });
+  }
+
+  handleGenreSelect = genre => {
+    this.setState({ selectedGenre: genre, currentPage: 1 });
+  };
+  handlePageChange = page => {
+    this.setState({ currentPage: page });
   };
 
   handleDelete = movie => {
     const movies = this.state.movies.filter(m => m._id !== movie._id);
     this.setState({ movies });
   };
+
   handleLike = movie => {
     const movies = [...this.state.movies];
     const index = movies.indexOf(movie);
@@ -18,48 +40,57 @@ class Movies extends Component {
     movies[index].liked = !movies[index].liked;
     this.setState({ movies });
   };
+
+  handleSort = sortColumn => {
+    this.setState({ sortColumn });
+  };
+
   render() {
     const { length: count } = this.state.movies;
+    const {
+      currentPage,
+      pageSize,
+      sortColumn,
+      movies: allMovies,
+      genres,
+      selectedGenre
+    } = this.state;
+
     if (count === 0) return <p>There are no movies in the database!</p>;
+    const filtered =
+      selectedGenre && selectedGenre._id
+        ? allMovies.filter(m => m.genre._id === selectedGenre._id)
+        : allMovies;
+
+    const sorted = _.orderBy(filtered, [sortColumn.path], [sortColumn.order]);
+    const movies = paginate(sorted, currentPage, pageSize);
+
     return (
-      <React.Fragment>
-        <p>Showing {count} movies in the database.</p>
-        <table className="table">
-          <thead>
-            <tr>
-              <th>Title</th>
-              <th>Genre</th>
-              <th>Stock</th>
-              <th>Rate</th>
-              <th />
-            </tr>
-          </thead>
-          <tbody>
-            {this.state.movies.map(movie => (
-              <tr key={movie._id}>
-                <td>{movie.title}</td>
-                <td>{movie.genre.name}</td>
-                <td>{movie.numberInStock}</td>
-                <td>{movie.dailyRentalRate}</td>
-                <td>
-                  <Like
-                    liked={movie.liked}
-                    onClick={() => this.handleLike(movie)}
-                  />
-                </td>
-                <td>
-                  <button
-                    onClick={() => this.handleDelete(movie)}
-                    className="btn btn-danger sm"
-                  >
-                    Delete
-                  </button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </React.Fragment>
+      <div className="row">
+        <div className="col-3">
+          <ListGroup
+            items={genres}
+            selectedItem={selectedGenre}
+            onItemSelect={this.handleGenreSelect}
+          />
+        </div>
+        <div className="col">
+          <p>Showing {filtered.length} movies in the database.</p>
+          <MoviesTable
+            movies={movies}
+            sortColumn={sortColumn}
+            onLike={this.handleLike}
+            onDelete={this.handleDelete}
+            onSort={this.handleSort}
+          />
+          <Pagination
+            itemsCount={filtered.length}
+            pageSize={pageSize}
+            currentPage={currentPage}
+            onPageChange={this.handlePageChange}
+          />
+        </div>
+      </div>
     );
   }
 }
