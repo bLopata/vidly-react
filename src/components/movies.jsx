06c716e3d28059af1db67121b25de2b771222441
bLopata <benlopata@gmail.com>
2018-11-04 03:27:58 -0600

    Added node backend integration.

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -4,9 +4,10 @@ import MoviesTable from "./moviesTable";
 import SearchBox from "./common/searchBox";
 import Pagination from "./common/pagination";
 import { paginate } from "../utils/paginate";
+import { toast } from "react-toastify";
 import ListGroup from "./common/listGroup";
-import { getMovies } from "../services/fakeMovieService";
-import { getGenres } from "../services/fakeGenreService";
+import { getMovies, deleteMovie } from "../services/movieService";
+import { getGenres } from "../services/genreService";
 import _ from "lodash";
 
 class Movies extends Component {
@@ -20,9 +21,11 @@ class Movies extends Component {
     sortColumn: { path: "title", order: "asc" }
   };
 
-  componentDidMount() {
-    const genres = [{ _id: "", name: "All Genres" }, ...getGenres()];
-    this.setState({ movies: getMovies(), genres });
+  async componentDidMount() {
+    const { data } = await getGenres();
+    const genres = [{ id: "", name: "All Genres" }, ...data];
+    const { data: movies } = await getMovies();
+    this.setState({ movies, genres });
   }
 
   handleGenreSelect = genre => {
@@ -36,9 +39,17 @@ class Movies extends Component {
     this.setState({ currentPage: page });
   };
 
-  handleDelete = movie => {
-    const movies = this.state.movies.filter(m => m._id !== movie._id);
+  handleDelete = async movie => {
+    const originalMovies = this.state.movies;
+    const movies = await originalMovies.filter(m => m._id !== movie._id);
     this.setState({ movies });
+    try {
+      await deleteMovie(movie._id);
+    } catch (ex) {
+      if (ex && ex.status === 404)
+        toast.error("This movie has already been deleted");
+      this.setStat({ movies: originalMovies });
+    }
   };
 
   handleLike = movie => {
